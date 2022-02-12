82eac25431c8c5d1ad81bab32683e297820828a3
Max <max.lin@screeningeagle.com>
2022-02-12 16:46:48 +0800

    Setup response interface

--- services/hotel/hotel.go
+++ services/hotel/hotel.go
@@ -1,9 +1,39 @@
 package hotel
 
 type Hotel struct {
-	ID          string `json:"id"`
-	Destination string `json:"destination"`
-	Name        string `json:"Name"`
+	ID                string    `json:"id"`
+	Destination       string    `json:"destination"`
+	Name              string    `json:"name"`
+	Location          Location  `json:"location"`
+	Description       string    `json:"description"`
+	Amenities         Amenities `json:"amenities"`
+	Images            Images    `json:"images"`
+	BoodingConditions []string  `json:"booking_conditions"`
+}
+
+type Location struct {
+	Lat     float32 `json:"lat"`
+	Lng     float32 `json:"lng"`
+	Address string  `json:"address"`
+	City    string  `json:"city"`
+	Contry  string  `json:"country"`
+}
+
+type Amenities struct {
+	General []string `json:"general"`
+	Room    []string `json:"room"`
+	Others  []string `json:"others"`
+}
+
+type Images struct {
+	Rooms     []ImageLink `json:"rooms"`
+	Site      []ImageLink `json:"site"`
+	Amenities []ImageLink `json:"amenities"`
+}
+
+type ImageLink struct {
+	Link        string `json:"link"`
+	Description string `json:"description"`
 }
 
 func GetHotels(destination string, hotelIDs []string) (hotels []Hotel, err error) {
